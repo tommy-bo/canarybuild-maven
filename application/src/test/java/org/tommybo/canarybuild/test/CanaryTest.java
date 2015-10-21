a32f7c5242bfedfb4193efcb30c62cd3cf01bb0d
Tommy Bø <tommy.bo@visma.com>
2015-10-21 16:07:00 +0200

    Initial working version

---
+++ application/src/test/java/org/tommybo/canarybuild/test/CanaryTest.java
@@ -0,0 +1,12 @@
+package org.tommybo.canarybuild.test;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CanaryTest {
+	@Test
+	public void unittests_running() {
+		assertThat(true).isTrue();
+	}
+}
